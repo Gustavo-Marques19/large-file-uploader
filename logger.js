30badd017d5de46c6476f69ae7760e2da6fdfbe8
Gustavo-Marques19 <gustavomarques508@gmail.com>
2022-03-15 00:06:51 -0300

    wip upload

--- logger.js
+++ logger.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
-const winston = require('winston');
-const level = process.env.LOG_LEVEL || 'debug';
-const silent = process.env.NODE_ENV == 'test';
+const winston = require("winston");
+const level = process.env.LOG_LEVEL || "debug";
+const silent = process.env.NODE_ENV == "test";
 
 const loggerFormat = winston.format.printf(({ level, message, timestamp }) => {
   return `{"level": "${level}", "timestamp": "${timestamp}", "message": "${message}"}`;
@@ -10,22 +10,17 @@ const loggerFormat = winston.format.printf(({ level, message, timestamp }) => {
 
 const logger = winston.createLogger({
   level,
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    loggerFormat
-  ),
-  transports: [
-    new winston.transports.Console()
-  ],
+  format: winston.format.combine(winston.format.timestamp(), loggerFormat),
+  transports: [new winston.transports.Console()],
   exitOnError: false,
-  silent
+  silent,
 });
 
 // Allow morgan middleware to write to winston
 const stream = {
   write: (message) => {
     logger.info(message.trim());
-  }
+  },
 };
 
 module.exports = logger;
